c1cb96de44ba8e2ef556025c6d0d80c59fdd22fd
flare-s <webflarez@gmail.com>
2022-09-19 15:59:57 +0100

    Finished challenge 4

--- src/server.js
+++ src/server.js
@@ -19,6 +19,7 @@ server.get("/", (req, res) => {
 server.get("/colour", (req, res) => {
   const hex = req.query.hex || "ffffff";
   res.send(`
+    <!DOCTYPE html>
     <html>
         <head>
         <meta charset="utf-8">
@@ -31,9 +32,28 @@ server.get("/colour", (req, res) => {
         </head>
         <body>
         <h1>Hello Express</h1>
-        <form method="get" action="/colour">
+        <form method="GET" action="/colour">
             <label for="hex">Insert a hex value</label>
             <input type="text" name="hex" id="hex"/>
+        </form>
+        </body>
+    </html>`);
+});
+
+server.get("/cheese", (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+        <head>
+        <meta charset="utf-8">
+        <title>Home</title>
+        </head>
+        <body>
+        <form method="POST" action="/cheese">
+            <label for="cheese-name">Cheese name</label>
+            <input type="text" name="cheeseName" id="cheese-name"/>
+            <label for="cheese-rate">Cheese rate</label>
+            <input type="range" min="0" max="5" name="cheeseRate" id="cheese-rate"/>
         </form>
         </body>
     </html>`);
