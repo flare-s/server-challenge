b59e4b5d84aa7b19542be26c8e9b2e127d2d40b5
flare-s <webflarez@gmail.com>
2022-09-19 15:07:56 +0100

    Finished first challenge

--- src/server.js
+++ src/server.js
@@ -2,4 +2,18 @@ const express = require("express");
 
 const server = express();
 
+server.get("/", (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+        <head>
+        <meta charset="utf-8">
+        <title>Home</title>
+        </head>
+        <body>
+        <h1>Hello Express</h1>
+        </body>
+    </html>`);
+});
+
 module.exports = server;
